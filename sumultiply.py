763afc9366aa50f4b7c152b2170f2a2eebb9b596
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-03-20 15:03:27 +0000

    Update sumultiply.py

--- sumultiply.py
+++ sumultiply.py
@@ -1,7 +1,10 @@
 def sumultiply(x, y):
+    #create a variable that will become the answer
     total = 0
+    #loop over y adding x to the answer
     for i in range(y):
         total = total + x
     return total
+#test with the below variables
 print(sumultiply(11, 13))
 print(sumultiply(5, 123))
